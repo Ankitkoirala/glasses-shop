40a62ea368934c565ef8ce8094f4a97c68155343
unknown <acekoirala@gmail.com>
2019-11-25 20:39:03 +0545

    Shp page and styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 import {Route,Switch} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
-const HatPage = ()=>(
-  <div>
-    <h1>
-      Hats Page
-    </h1>
-  </div>
-)
+
 
 function App() {
   return (
@@ -18,7 +13,7 @@ function App() {
     {/* <HomePage/> */}
     <Switch>
     <Route exact path='/' component={HomePage} />
-    <Route path='/hats' component={HatPage} />
+    <Route path='/shop' component={ShopPage} />
     </Switch>
     </div>
 
