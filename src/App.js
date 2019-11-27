45e4452d7cbfb0bfed20ff648dc248e88ad9ec27
unknown <acekoirala@gmail.com>
2019-11-27 15:48:06 +0545

    adding sigin componenet and page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import {Route,Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
@@ -15,6 +16,8 @@ function App() {
     <Switch>
     <Route exact path='/' component={HomePage} />
     <Route path='/shop' component={ShopPage} />
+    <Route path='/signin' component={SignInAndSignUpPage} />
+
     </Switch>
     </div>
 
