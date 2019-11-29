09541cae6163809f374cb9f0396cce3db2750bee
unknown <acekoirala@gmail.com>
2019-11-29 17:15:50 +0545

    signIn and SignUP using firebase utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import {Route,Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils'
+import {auth,createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
 constructor(){
@@ -20,13 +20,27 @@ constructor(){
 unsubscrbeFromAuth=null
 
 componentDidMount(){
- this.unsubscrbeFromAuth= auth.onAuthStateChanged(user=>{
-    this.setState({
-      currentUser:user
-    })
-    console.log(user);
+ this.unsubscrbeFromAuth= auth.onAuthStateChanged(async userAuth=>{
+if(userAuth){
+  const userRef = await createUserProfileDocument(userAuth)
+  userRef.onSnapshot(snapShot =>{
+this.setState({
+  currentUser:{
+    id:snapShot.id,
+    ...snapShot.data()
+  }
+})
+  console.log(this.state);
   })
 
+ 
+
+}
+else{
+  this.setState({currentUser:userAuth})
+}
+  })
+ 
 }
 componentWillUnmount(){
   this.unsubscrbeFromAuth();
